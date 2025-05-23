d8a2a5a9e42c6eb49fd21d723f715429d04156de
dariolpz05 <aodariolopez@gmail.com>
2025-05-23 12:30:43 +0200

    Añadiendo proyectos y modificando estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent, AboutComponent],
+  imports: [RouterOutlet, HeroComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
